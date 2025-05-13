45bc55ddb37df936706b416237ce79c7d4eb63c8
urvashi2594 <dhakeurvashi7@gmail.com>
2025-05-13 09:07:08 -0400

    Like and Dislike functionality implemented

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,37 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0, 
+        dislike: 0,
+        handleLike: () => {
+        this.setState((prevState) => {
+            likes: prevState.likes+1;
+        });
+        },
+        handleDislike: () => {
+            this.setState((prevState) => {
+                dislike: prevState.dislike+1;
+            });
+        },
+    };
+
   }
+  
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+        <div className='content-rating'>
+            <p>React is mostly used framework for front-end devlopment and it's most popular now days</p>
+            <div className='rating-buttons'>
+                <button className='like-button' onClick={this.state.handleLike}>
+                    Like ({this.state.likes})
+                </button>
+                <button className='dislike-button' onClick={this.state.handleDislike}>
+                    Dislike ({this.state.likes})
+                </button>
+            </div>
+        </div>
      </>
     );
   }
